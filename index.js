58c2021429fb942eda6827cb06e77d2dff29f390
TracyAntonia <aantonjuguna@gmail.com>
2023-07-08 23:43:20 +0300

    completed assignments

---
+++ index.js
@@ -0,0 +1,5 @@
+const key = "RZEIOVfPhS7vMLkFdd2TSKGFBS4o9_FmcV1Nje3FSjw";
+
+const form = document.querySelector("form");
+const inputSearch = docuument.querySelectorById("input-search");
+const results = document.querySelector(".results")
