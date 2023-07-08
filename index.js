a3c49fdc78d1616a80d1a3851c894991c8de066e
TracyAntonia <aantonjuguna@gmail.com>
2023-07-08 23:48:48 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const key = "RZEIOVfPhS7vMLkFdd2TSKGFBS4o9_FmcV1Nje3FSjw";
 
 const form = document.querySelector("form");
-const inputSearch = docuument.querySelectorById("input-search");
-const results = document.querySelector(".results")
+const inputSearch = docuument.getElementById("input-search");
+const results = document.querySelector(".results");
+const moreImg = document.getElementById("more-img");
+
+function imageSearch() {
+    inputData = inputSearch.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+}
